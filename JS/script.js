b8815a4542be3fda9de6bd98b57d087dd66fa98b
thlemez <matheuslemesamgmail.com>
2024-11-28 16:06:54 -0300

    site portifólio

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+document.getElementById('form-contato').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+    this.reset();
+});
